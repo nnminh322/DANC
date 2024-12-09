e22476f5a1fb662bd388c10994f8b54771e1bf98
nnminh322 <nnminh322@gmail.com>
2024-12-10 05:36:13 +0700

    Update data_loader.py

--- FCRE/CPL/data_loader.py
+++ FCRE/CPL/data_loader.py
@@ -81,7 +81,7 @@ class BERTLLMDataset(Dataset):
 
     def collate_fn(self, data):
         # print(data)
-        print(self.config.model_dir)
+        print(f'model_dir: {self.config.model_dir}')
         batch_instance = {'input': [],'ids': [], 'mask': []} 
         batch_label = []
         batch_idx = []
