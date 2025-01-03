531e70ac502a97d8e0dec9541829c381f9ae020e
nnminh322 <nnminh322@gmail.com>
2025-01-03 11:10:33 +0700

    Update gsam.py

--- FCED/gsam/gsam.py
+++ FCED/gsam/gsam.py
@@ -3,6 +3,7 @@ import contextlib
 from torch.distributed import ReduceOp
 from torch.nn.modules.batchnorm import _BatchNorm
 
+
 def disable_running_stats(model):
     def _disable(module):
         if isinstance(module, _BatchNorm):
@@ -11,6 +12,7 @@ def disable_running_stats(model):
 
     model.apply(_disable)
 
+
 def enable_running_stats(model):
     def _enable(module):
         if isinstance(module, _BatchNorm) and hasattr(module, "backup_momentum"):
@@ -18,8 +20,20 @@ def enable_running_stats(model):
 
     model.apply(_enable)
 
+
 class GSAM(torch.optim.Optimizer):
-    def __init__(self, params, base_optimizer, model, gsam_alpha, rho_scheduler, adaptive=False, perturb_eps=1e-12, grad_reduce='mean', **kwargs):
+    def __init__(
+        self,
+        params,
+        base_optimizer,
+        model,
+        gsam_alpha,
+        rho_scheduler,
+        adaptive=False,
+        perturb_eps=1e-12,
+        grad_reduce="mean",
+        **kwargs
+    ):
         defaults = dict(adaptive=adaptive, **kwargs)
         super(GSAM, self).__init__(params, defaults)
         self.model = model
@@ -29,24 +43,24 @@ class GSAM(torch.optim.Optimizer):
         self.rho_scheduler = rho_scheduler
         self.perturb_eps = perturb_eps
         self.alpha = gsam_alpha
-        
+
         # initialize self.rho_t
         self.update_rho_t()
-        
+
         # set up reduction for gradient across workers
-        if grad_reduce.lower() == 'mean':
-            if hasattr(ReduceOp, 'AVG'):
+        if grad_reduce.lower() == "mean":
+            if hasattr(ReduceOp, "AVG"):
                 self.grad_reduce = ReduceOp.AVG
                 self.manual_average = False
-            else: # PyTorch <= 1.11.0 does not have AVG, need to manually average across processes
+            else:  # PyTorch <= 1.11.0 does not have AVG, need to manually average across processes
                 self.grad_reduce = ReduceOp.SUM
                 self.manual_average = True
-        elif grad_reduce.lower() == 'sum':
+        elif grad_reduce.lower() == "sum":
             self.grad_reduce = ReduceOp.SUM
             self.manual_average = False
         else:
             raise ValueError('"grad_reduce" should be one of ["mean", "sum"].')
-    
+
     @torch.no_grad()
     def update_rho_t(self):
         self.rho_t = self.rho_scheduler.step()
@@ -54,57 +68,83 @@ class GSAM(torch.optim.Optimizer):
 
     @torch.no_grad()
     def perturb_weights(self, rho=0.0):
-        grad_norm = self._grad_norm( weight_adaptive = self.adaptive )
+        grad_norm = self._grad_norm(weight_adaptive=self.adaptive)
         for group in self.param_groups:
             scale = rho / (grad_norm + self.perturb_eps)
 
             for p in group["params"]:
-                if p.grad is None: continue
+                if p.grad is None:
+                    continue
                 self.state[p]["old_g"] = p.grad.data.clone()
                 e_w = p.grad * scale.to(p)
                 if self.adaptive:
                     e_w *= torch.pow(p, 2)
                 p.add_(e_w)  # climb to the local maximum "w + e(w)"
-                self.state[p]['e_w'] = e_w
-                
+                self.state[p]["e_w"] = e_w
+
     @torch.no_grad()
     def unperturb(self):
         for group in self.param_groups:
-            for p in group['params']:
-                if 'e_w' in self.state[p].keys():
-                    p.data.sub_(self.state[p]['e_w'])
+            for p in group["params"]:
+                if "e_w" in self.state[p].keys():
+                    p.data.sub_(self.state[p]["e_w"])
 
     @torch.no_grad()
     def gradient_decompose(self, alpha=0.0):
         # calculate inner product
         inner_prod = 0.0
         for group in self.param_groups:
-            for p in group['params']:
-                if p.grad is None: continue
-                inner_prod += torch.sum(
-                    self.state[p]['old_g'] * p.grad.data
-                )
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                inner_prod += torch.sum(self.state[p]["old_g"] * p.grad.data)
 
         # get norm
         new_grad_norm = self._grad_norm()
-        old_grad_norm = self._grad_norm(by='old_g')
+        old_grad_norm = self._grad_norm(by="old_g")
 
         # get cosine
         cosine = inner_prod / (new_grad_norm * old_grad_norm + self.perturb_eps)
 
         # gradient decomposition
         for group in self.param_groups:
-            for p in group['params']:
-                if p.grad is None: continue
-                vertical = self.state[p]['old_g'] - cosine * old_grad_norm * p.grad.data / (new_grad_norm + self.perturb_eps)
-                p.grad.data.add_( vertical, alpha=-alpha)
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                vertical = self.state[p][
+                    "old_g"
+                ] - cosine * old_grad_norm * p.grad.data / (
+                    new_grad_norm + self.perturb_eps
+                )
+                p.grad.data.add_(vertical, alpha=-alpha)
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                self.state[p]["old_p"] = p.data.clone()
+                e_w = (
+                    (torch.pow(p, 2) if group["adaptive"] else 1.0)
+                    * p.grad
+                    * scale.to(p)
+                )
+                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+
+        if zero_grad:
+            self.zero_grad()
 
     @torch.no_grad()
     def _sync_grad(self):
-        if torch.distributed.is_initialized(): # synchronize final gardients
+        if torch.distributed.is_initialized():  # synchronize final gardients
             for group in self.param_groups:
-                for p in group['params']:
-                    if p.grad is None: continue
+                for p in group["params"]:
+                    if p.grad is None:
+                        continue
                     if self.manual_average:
                         torch.distributed.all_reduce(p.grad, op=self.grad_reduce)
                         world_size = torch.distributed.get_world_size()
@@ -115,31 +155,42 @@ class GSAM(torch.optim.Optimizer):
 
     @torch.no_grad()
     def _grad_norm(self, by=None, weight_adaptive=False):
-        #shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        # shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
         if not by:
             norm = torch.norm(
-                    torch.stack([
-                        ( (torch.abs(p.data) if weight_adaptive else 1.0) *  p.grad).norm(p=2)
-                        for group in self.param_groups for p in group["params"]
+                torch.stack(
+                    [
+                        ((torch.abs(p.data) if weight_adaptive else 1.0) * p.grad).norm(
+                            p=2
+                        )
+                        for group in self.param_groups
+                        for p in group["params"]
                         if p.grad is not None
-                    ]),
-                    p=2
-               )
+                    ]
+                ),
+                p=2,
+            )
         else:
             norm = torch.norm(
-                torch.stack([
-                    ( (torch.abs(p.data) if weight_adaptive else 1.0) * self.state[p][by]).norm(p=2)
-                    for group in self.param_groups for p in group["params"]
-                    if p.grad is not None
-                ]),
-                p=2
+                torch.stack(
+                    [
+                        (
+                            (torch.abs(p.data) if weight_adaptive else 1.0)
+                            * self.state[p][by]
+                        ).norm(p=2)
+                        for group in self.param_groups
+                        for p in group["params"]
+                        if p.grad is not None
+                    ]
+                ),
+                p=2,
             )
         return norm
 
     def load_state_dict(self, state_dict):
         super().load_state_dict(state_dict)
         self.base_optimizer.param_groups = self.param_groups
-        
+
     def maybe_no_sync(self):
         if torch.distributed.is_initialized():
             return self.model.no_sync()
@@ -189,9 +240,9 @@ class GSAM(torch.optim.Optimizer):
 
             # unperturb
             self.unperturb()
-            
+
         # synchronize gradients across workers
-        self._sync_grad()    
+        self._sync_grad()
 
         # update with new directions
         self.base_optimizer.step()
